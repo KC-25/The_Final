efb8512291e309470a5668463474b271dff4f1f3
KC-25 <kierrconnell@g.crookcountyschools.org>
2023-06-13 10:17:34 -0700

    Working on earth.html

--- js/script.js
+++ js/script.js
@@ -24,27 +24,35 @@ ocean.addEventListener('click', handleButtonClickO);
 under.addEventListener('click', handleButtonClickU);
 
 function handleButtonClickS(eventS){
+    function skycharacter(){
+        location.replace("/sky.html");
+    }
     (eventS.target.id === sky)
         console.log('sky');
-        window.location.href = "game.html";
-        document.write('<h6>You have chosen the gods of Olympus. Your compainion is Aether.');
-}
+        skycharacter();
+    }
 function handleButtonClickE(eventE){
     (eventE.target.id === earth)
+    function earthcharacter(){
+        location.replace("/earth.html");
+    }
         console.log('earth');
-        window.location.href = "game.html";
-        document.write('<h6>You have chosen the gods of The Mortal Realm. Your compainion is Gaius.'); 
+        earthcharacter();
 }
 function handleButtonClickO(eventO){
     (eventO.target.id === ocean)
+    function oceancharacter(){
+        location.replace("/ocean.html");
+    }
         console.log('ocean');
-        window.location.href = "game.html";
-        document.write('<h6>You have chosen the gods of The Ocean. Your compainion is Melia.');
+        oceancharacter();
 }
 function handleButtonClickU(eventU){
     (eventU.target.id === under)
+    function undercharacter(){
+        location.replace("/under.html");
+    }
         console.log('under');
-        window.location.href = "game.html";
-        document.write('<h6>You have chosen the gods of The Underworld. Your compainion is Styx.');
+        undercharacter();
 }
     
